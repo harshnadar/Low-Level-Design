15f8ffda64928d7a60dd4301e6e0fe84840854c8
harshnadar <harshnadar.eee18@itbhu.ac.in>
2026-09-24 20:12:59 +0530

    trying the commit from a new location

--- design_principles/decorator_pattern/concrete_decorators.py
+++ design_principles/decorator_pattern/concrete_decorators.py
@@ -8,6 +8,7 @@ class Mocha(CondimentDecorator):
     def cost(self) -> float:
         return self.beverage.cost() + 0.49  # Add mocha cost
 
+
 class Whip(CondimentDecorator):
     def get_description(self) -> str:
         return f"{self.beverage.get_description()}, Whip"
